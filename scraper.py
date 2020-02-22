447b5d045a7664d7bf051cb5a02182896cb287ca
Logan Cooper <ldtcoop@gmail.com>
2020-02-22 15:39:45 -0800

    Add type hints

--- scraper.py
+++ scraper.py
@@ -1,10 +1,14 @@
 import requests
-from bs4 import BeautifulSoup
+import bs4
 import re
 import operator
+from typing import Tuple, Optional, List
 
+# types
+ResultSet = bs4.element.ResultSet
+Tag = bs4.element.Tag
 
-def filter_html(tag):
+def filter_html(tag: Tag) -> bool:
     # gets all p tags with no i tags in them
     if tag.name == 'p' and not tag.i:
         if re.match('\([A-Z ]+\)', tag.text):
@@ -14,33 +18,33 @@ def filter_html(tag):
     else:
         return False
 
-def match_name_and_dialog(tag_text):
+def match_name_and_dialog(tag_text: str) -> Optional[bool]:
         # grabs the name of the speaker
         tag_matcher = '^(([A-Z]+)( \(\?\))?): (.+)$'
         return re.match(tag_matcher, tag_text)
 
-def extract_name_and_dialog(p_tag, last_match):
+def extract_name_and_dialog(p_tag: Tag, last_match: str) -> Tuple[str, str, bool]:
     tag_text = p_tag.string.replace('\n', '')
     speaker_match = match_name_and_dialog(tag_text)
     if speaker_match:
         # if there is a name in front, we know there is a new speaker
         name = speaker_match.group(2).lower()
-        replace_match = True
         dialog = speaker_match.group(4)
+        replace_match = True
     else:
         # otherwise, use the last name we saw
         name = last_match
-        replace_match = False
         dialog = tag_text
+        replace_match = False
     # transcript uses "health care" and "healthcare" interchangably
     return (name, dialog.replace('health care', 'healthcare'), replace_match)
 
-def get_html(url):
+def get_html(url: str) -> ResultSet:
     html_string = requests.get(url).content
-    html_tree = BeautifulSoup(html_string, 'html.parser')
+    html_tree = bs4.BeautifulSoup(html_string, 'html.parser')
     return html_tree.find('article').find_all(filter_html)
 
-def debate_pipeline(url, moderators):
+def debate_scraper(url: str, moderators: List[str]) -> List[dict]:
     p_collection = get_html(url)
     debate_collection = []
     last_match = None
@@ -53,4 +57,4 @@ def debate_pipeline(url, moderators):
         debate_collection.append({'speaker': name, 'dialog': dialog, 'moderator': (name in moderators)})
     return debate_collection
 
-print(debate_pipeline('https://www.washingtonpost.com/politics/2019/07/31/transcript-first-night-second-democratic-debate/', ['tapper', 'bash', 'lemon']))
+debate_scraper('https://www.washingtonpost.com/politics/2019/07/31/transcript-first-night-second-democratic-debate/', ['tapper', 'bash', 'lemon'])
