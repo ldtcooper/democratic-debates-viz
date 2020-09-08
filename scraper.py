c0cee3c4f8ffbf7b19a8a1d61566c920c05835a5
ldtcoop <ldtcoop@gmail.com>
2020-09-08 15:53:49 -0700

    Remove moderator classification and expand to all debates

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import pandas as pd
 import bs4
 from typing import List, Dict
 import re
+import csv
 
 Tag = bs4.element.Tag
 
@@ -40,56 +41,52 @@ def split_tag(tag: Tag) -> Dict[str, str]:
         # No <br> means we can't unpack. Should only happen for non-dialog
         return None
 
-
-
-def determine_moderator(speaker: str, moderators: List[str]) -> bool:
-    return True if speaker in moderators else False
-
-def convert_transcrit_to_list(transcript: List[Tag], moderators: List[str], debate_num: int) -> List[Dict[str, str]]:
+def convert_transcrit_to_list(transcript: List[Tag], debate_num: int) -> List[Dict[str, str]]:
     dialog_list = []
     for p in transcript:
         row = split_tag(p)
         if row == None:
             # we'll just skip non dialog rows
             continue
-        row['is_moderator'] = determine_moderator(row['speaker'], moderators)
         row['debate_num'] = debate_num
         dialog_list.append(row)
     return dialog_list
 
-def process_transcript(url: str, moderators: List[str], debate_num: int) -> List[Dict[str, str]]:
+def process_transcript(url: str, debate_num: int) -> List[Dict[str, str]]:
     transcript = get_tags_from_url(url)
-    return  convert_transcrit_to_list(transcript, moderators, debate_num)
+    return  convert_transcrit_to_list(transcript, debate_num)
+
+def make_csv(transcripts: List[Dict[str, str]]):
+    # adapted from https://stackoverflow.com/questions/3086973/how-do-i-convert-this-list-of-dictionaries-to-a-csv-file
+    headers = transcripts[0].keys()
+    with open('debates.csv', 'w', encoding='utf8', newline='') as output:
+        dict_writer = csv.DictWriter(output, headers)
+        dict_writer.writeheader()
+        dict_writer.writerows(transcripts)
+
 
 transcript_sources = [
-    {
-        'url': 'https://www.rev.com/blog/transcripts/transcript-from-first-night-of-democratic-debates',
-        # moderator list source: https://www.nbcnews.com/politics/2020-election/nbc-announces-five-moderators-first-democratic-debate-n1016106
-        'moderators': [
-            'Lester Holt',
-            'Savannah G.',
-            'Chuck Todd',
-            'Rachel Maddow',
-            'Jose D.B.'
-        ]
-    },
-    {
-        'url': 'https://www.rev.com/blog/transcripts/transcript-from-night-2-of-the-2019-democratic-debates',
-        # moderator list source: https://www.nbcnews.com/politics/2020-election/nbc-announces-five-moderators-first-democratic-debate-n1016106
-        'moderators': [
-            'Lester Holt',
-            'Savannah G.',
-            'Chuck Todd',
-            'Rachel Maddow',
-            'Jose D.B.'
-        ]
-    },
+    'https://www.rev.com/blog/transcripts/transcript-from-first-night-of-democratic-debates',
+    'https://www.rev.com/blog/transcripts/transcript-from-night-2-of-the-2019-democratic-debates',
+    'https://www.rev.com/blog/transcripts/transcript-of-july-democratic-debate-night-1-full-transcript-july-30-2019',
+    'https://www.rev.com/blog/transcripts/transcript-of-july-democratic-debate-2nd-round-night-2-full-transcript-july-31-2019',
+    'https://www.rev.com/blog/transcripts/democratic-debate-transcript-houston-september-12-2019',
+    'https://www.rev.com/blog/transcripts/october-democratic-debate-transcript-4th-debate-from-ohio',
+    'https://www.rev.com/blog/transcripts/november-democratic-debate-transcript-atlanta-debate-transcript',
+    'https://www.rev.com/blog/transcripts/december-democratic-debate-transcript-sixth-debate-from-los-angeles',
+    'https://www.rev.com/blog/transcripts/january-iowa-democratic-debate-transcript',
+    'https://www.rev.com/blog/transcripts/new-hampshire-democratic-debate-transcript',
+    'https://www.rev.com/blog/transcripts/democratic-debate-transcript-las-vegas-nevada-debate',
+    'https://www.rev.com/blog/transcripts/south-carolina-democratic-debate-transcript-february-democratic-debate',
+    'https://www.rev.com/blog/transcripts/march-democratic-debate-transcript-joe-biden-bernie-sanders'
 ]
 
 transcript_output = []
 for i in range(len(transcript_sources)):
-    src = transcript_sources[i]
-    transcript = process_transcript(src['url'], src['moderators'], i + 1)
+    url = transcript_sources[i]
+    transcript = process_transcript(url, i + 1)
     transcript_output += transcript
 
-print(transcript_output)
+make_csv(transcript_output)
+
+
