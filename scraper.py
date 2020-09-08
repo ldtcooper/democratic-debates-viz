54e1c7067270a48fbd05da908c8a623d55773f4f
ldtcoop <ldtcoop@gmail.com>
2020-09-08 15:18:20 -0700

    Get scraper working for second night of first debate

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ import re
 
 Tag = bs4.element.Tag
 
+# we're going to be removing a lot of things between brackets
 bracket_text = re.compile(r'\[.+\]')
 
 
@@ -25,44 +26,70 @@ def remove_bracket_text(dialog: str) -> str:
     return clean_string
 
 
-def split_tag(tag: Tag) -> List[str]:
+def split_tag(tag: Tag) -> Dict[str, str]:
     """
     The speaker is split from their dialog by a single <br/>
     This function splits each p tag based on that
-    """
-    speaker, dialog = tag.decode_contents().split('<br/>')
-    # remove colon from after speaker name
-    speaker = speaker[:-1]
-    return [speaker, remove_bracket_text(dialog)]
+    """ 
+    try:
+        speaker, dialog = tag.decode_contents().split('<br/>')
+        # remove everything after colon from after speaker name
+        speaker = speaker.split(':')[0]
+        return {'speaker': speaker, 'dialog': remove_bracket_text(dialog)}
+    except ValueError as e:
+        # No <br> means we can't unpack. Should only happen for non-dialog
+        return None
 
 
-def convert_transcrit_to_list(transcript: List[Tag]) -> List[List[str]]:
+
+def determine_moderator(speaker: str, moderators: List[str]) -> bool:
+    return True if speaker in moderators else False
+
+def convert_transcrit_to_list(transcript: List[Tag], moderators: List[str], debate_num: int) -> List[Dict[str, str]]:
     dialog_list = []
     for p in transcript:
         row = split_tag(p)
+        if row == None:
+            # we'll just skip non dialog rows
+            continue
+        row['is_moderator'] = determine_moderator(row['speaker'], moderators)
+        row['debate_num'] = debate_num
         dialog_list.append(row)
     return dialog_list
 
+def process_transcript(url: str, moderators: List[str], debate_num: int) -> List[Dict[str, str]]:
+    transcript = get_tags_from_url(url)
+    return  convert_transcrit_to_list(transcript, moderators, debate_num)
 
-def add_moderator_row(data: List[List[str]], moderators: List[str]) -> List[List[str]]:
-    for row in data:
-        speaker = row[0]
-        is_moderator = speaker in moderators
-        row.append(is_moderator)
-    return data
-
-
-url = 'https://www.rev.com/blog/transcripts/transcript-from-first-night-of-democratic-debates'
-
-# moderator list source: https://www.nbcnews.com/politics/2020-election/nbc-announces-five-moderators-first-democratic-debate-n1016106
-moderators = [
-    'Lester Holt',
-    'Savannah G.',
-    'Chuck Todd',
-    'Rachel Maddow',
-    'Jose D.B.',
+transcript_sources = [
+    {
+        'url': 'https://www.rev.com/blog/transcripts/transcript-from-first-night-of-democratic-debates',
+        # moderator list source: https://www.nbcnews.com/politics/2020-election/nbc-announces-five-moderators-first-democratic-debate-n1016106
+        'moderators': [
+            'Lester Holt',
+            'Savannah G.',
+            'Chuck Todd',
+            'Rachel Maddow',
+            'Jose D.B.'
+        ]
+    },
+    {
+        'url': 'https://www.rev.com/blog/transcripts/transcript-from-night-2-of-the-2019-democratic-debates',
+        # moderator list source: https://www.nbcnews.com/politics/2020-election/nbc-announces-five-moderators-first-democratic-debate-n1016106
+        'moderators': [
+            'Lester Holt',
+            'Savannah G.',
+            'Chuck Todd',
+            'Rachel Maddow',
+            'Jose D.B.'
+        ]
+    },
 ]
 
-transcript = get_tags_from_url(url)
-dialog_list = convert_transcrit_to_list(transcript)
-print(add_moderator_row(dialog_list, moderators))
+transcript_output = []
+for i in range(len(transcript_sources)):
+    src = transcript_sources[i]
+    transcript = process_transcript(src['url'], src['moderators'], i + 1)
+    transcript_output += transcript
+
+print(transcript_output)
